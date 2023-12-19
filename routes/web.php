041ce71ab02bc421b0a61e137f861e218b2eb7a4
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-12-19 12:32:14 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/9mm/public/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/9mm/public/livewire/update', $handle);
+});
